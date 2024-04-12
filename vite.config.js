1f6ecc13b31b527d3417e16a30d241534687c3d9
ajea <smithajea@rocketmail.com>
2024-04-12 15:19:24 -0400

    added vite favicon

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
 			input: {
 				main: resolve(__dirname, "index.html"),
 				about: resolve(__dirname, "about/index.html"),
+				contact: resolve(__dirname, "contact/index.html"),
 				app: resolve(__dirname, "app/index.html"),
 				locations: resolve(__dirname, "locations/index.html"),
 				graphic: resolve(__dirname, "graphic/index.html"),
