dd6e9bfb91bb17a0e269c3d65f8a6824ae85a009
ajea <smithajea@rocketmail.com>
2024-04-12 14:52:54 -0400

    corrected file  paths

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
 			input: {
 				main: resolve(__dirname, "index.html"),
 				about: resolve(__dirname, "about/index.html"),
+				app: resolve(__dirname, "app/index.html"),
+				locations: resolve(__dirname, "locations/index.html"),
+				graphic: resolve(__dirname, "graphic/index.html"),
+				web: resolve(__dirname, "web/index.html"),
 			},
 		},
 	},
